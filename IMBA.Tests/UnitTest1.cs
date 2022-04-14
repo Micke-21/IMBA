f6472d4d2165e335346a27e14cf32fb322d83bce
Mikael Larsson <micke.lar@gmail.com>
2022-04-14 13:08:59 +0200

    Add som things

---
+++ IMBA.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IMBA.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ IMBA/Models/Im.cs
@@ -0,0 +1,14 @@
+﻿namespace IMBA.Models
+{
+    public class Im
+    {
+        public Guid Id { get; set; } 
+
+        public string Name { get; set; }
+
+        public string? Status { get; set; }
+
+        public string Data { get; set; }
+
+    }
+}
